a513993da6efd6ec8fa650dea988fda21b4184ff
Antony Vennard <antony@vennard.ch>
2022-03-14 16:05:39 +0100

    Correct layout.

--- nrf52-ncs-52840dk-bluetooth/env.sh
+++ nrf52-ncs-52840dk-bluetooth/env.sh
@@ -1,3 +1,2 @@
 export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
 export GNUARMEMB_TOOLCHAIN_PATH=/opt/arm/arm-eabi-latest/
-export ZEPHYR_BASE=$(pwd)/zephyr
--- nrf52-ncs-52840dk-bluetooth/scripts/build.sh
+++ nrf52-ncs-52840dk-bluetooth/scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source scripts/env
-west build -s application -b nrf52840dk_nrf52840
+west build application -b nrf52840dk_nrf52840
